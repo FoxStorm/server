fa57492ff519afe5a927f1edc40abdf860ea26ec
Sebastian Virlan <sebastian.virlan@bbc.co.uk>
2019-02-06 23:23:38 +0000

    Regenerate dist

--- dist/src/ApplicationServer.d.ts
+++ dist/src/ApplicationServer.d.ts
@@ -1,5 +1,5 @@
 interface Server {
-    listen(port: number, callback: () => void): void;
+    listen(port: number, callback?: () => void): void;
     useRouter(router: any): void;
     setViewEngine(templateEngine: string): void;
 }
@@ -7,7 +7,7 @@ export declare class ApplicationServer implements Server {
     private readonly server;
     constructor(server?: any);
     useRouter(router: any): void;
-    listen(port: number, callback: () => void): void;
+    listen(port: number, callback?: () => void): void;
     setViewEngine(templateEngine: string): void;
 }
 export {};
